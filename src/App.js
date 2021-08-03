2c987be2dc841dc42054b2076214d1e9c697bc53
Setareh Khoylow <setarehkhoylow@Setarehs-MacBook-Pro.local>
2021-08-02 17:00:58 -0700

    Created search box component and styled project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 
 import {CardList} from './components/card-list/card-list.compenent';
+import {SearchBox} from './components/search-box/search-box.component';
+
 
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
+
+    // this.handleChange = this.handleChange.bind(this); // this allows our function to recognize the this keyword in our new class method
   }
 
   componentDidMount(){
@@ -18,34 +23,27 @@ class App extends Component{
       .then(users => this.setState({ monsters: users}))
   }
 
+  handleChange = (e) => { //arrow functions automatically bind 'this' to the context where it was given its existence which is the App component
+    this.setState({ searchField: e.target.value });
+  }
+
   render(){
+    const { monsters, searchField } = this.state; //destructuring ==> const monsters=this.state.monsters
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='Search Monster' 
+          handleChange={this.handleChange} 
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
